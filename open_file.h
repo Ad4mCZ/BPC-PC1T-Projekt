d3029a5eba090a3b76888417c5107d4e6d6a8a77
Ad4mCZ <biciste.adam@ssakhk.cz>
2022-12-03 19:28:27 +0100

    ukladani spravnych odpovedi

--- open_file.h
+++ open_file.h
@@ -3,6 +3,7 @@
 char data[MAX_LINES][MAX_LENGTH] = {{0}}; // 2d pole pro uchovavani radku
 char otazky[MAX_LINES][MAX_LENGTH] = {{0}};
 char odpovedi[MAX_LINES][MAX_LENGTH] = {{0}};
+char spravne_odpovedi[MAX_LINES][MAX_LENGTH] = {{0}};
 
 void Open_File(char *filename)
 {
@@ -40,7 +41,22 @@ void Open_File(char *filename)
             {
                 for (int k = 1; data[i][j + k] != ')'; k++)
                 {
+
                     odpovedi[indexOdpovedi][k - 1] = data[i][j + k];
+                }
+                indexOdpovedi++;
+            }
+        }
+    }
+    for (int i = 0, indexOdpovedi = 0; i < sizeof(odpovedi) / sizeof(odpovedi[0]); i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (odpovedi[i][j] == '!')
+            {
+                for (int k = 0; odpovedi[i][j + k] != '\0'; k++)
+                {
+                    spravne_odpovedi[indexOdpovedi][k - 1] = odpovedi[i][j + k];
                 }
                 indexOdpovedi++;
             }
